098aeb417e655b724fb15de47d2fc2c4962660f3
EleevateMX <edronemid@gmail.com>
2026-05-06 15:14:56 -0600

    Update data.js

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ const USERS = {
   crescencio_gutierrez:{ name: 'Cresencio Gutiérrez', password: 'CresencioBH2026', color:'#F59E0B', role:'admin', allowedPrograms:['educacion'] },
   ivan_herrera:       { name: 'Iván Herrera',       password: 'IvanBH2026',       color:'#8B5CF6', role:'admin', allowedPrograms:['deporte'] }
 };
-const APPS_SCRIPT_URL = '';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwqDq6v_wpk7V-0OQTOHJYXv7JFm1UF6EuQkuTw11gLaNvv_0DXYCXmppCWhVEz4Tgoqw/exec';
 
 /* CATÁLOGO BASE · SUBDIRECCIÓN DE EDUCACIÓN
    Bibliotecas/talleres cargados como estructura inicial. Los indicadores se alimentarán cuando envíen las cifras. */
